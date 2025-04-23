a054ea95598df00053954ed726fc0171df6a3fb1
ahmedabdelrhman2003 <ahmedabdelrhman297@gmail.com>
2025-04-23 17:17:12 +0200

    craete contact us api

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use App\Repositories\BookRepository;
 use App\Repositories\CategoryRepository;
+use App\Repositories\ContactUsRepository;
 use App\Repositories\Interfaces\BookRepositoryInterface;
 use App\Repositories\Interfaces\CategoryRepositoryInterface;
+use App\Repositories\Interfaces\ContactUsRepositoryInterface;
 use App\Repositories\Interfaces\UserRepositoryInterface;
 use App\Repositories\Interfaces\UserVerificationRepositoryInterface;
 use App\Repositories\UserRepository;
@@ -23,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(UserVerificationRepositoryInterface::class, UserVerificationRepository::class);
         $this->app->bind(BookRepositoryInterface::class, BookRepository::class);
         $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
+        $this->app->bind(ContactUsRepositoryInterface::class, ContactUsRepository::class);
     }
 
     /**
