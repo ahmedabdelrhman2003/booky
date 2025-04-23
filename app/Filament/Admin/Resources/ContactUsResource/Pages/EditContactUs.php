f0f90afa7ab3fdcd93c69f1b7f4a2da31e6d8077
ahmedabdelrhman2003 <ahmedabdelrhman297@gmail.com>
2025-04-23 19:30:14 +0200

    add contact us resource

---
+++ app/Filament/Admin/Resources/ContactUsResource/Pages/EditContactUs.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Admin\Resources\ContactUsResource\Pages;
+
+use App\Filament\Admin\Resources\ContactUsResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditContactUs extends EditRecord
+{
+    protected static string $resource = ContactUsResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+        ];
+    }
+}
